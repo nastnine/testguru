e43be793a786097d0a1ded8890ab758a56557477
Anastasia Ogorodnikova <nasti28121992@icloud.com>
2022-09-26 00:19:06 +0300

    Add

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,9 @@ module TestGuru
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Moscow"
     # config.eager_load_paths << Rails.root.join("extras")
+    # Permitted locales available for the application
+    I18n.available_locales = [:en, :ru]
   end
 end
